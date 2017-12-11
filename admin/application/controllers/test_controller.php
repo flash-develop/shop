004f2499d293249891dccab0949755fea5e32a7a
Sergey <sbratyshov@gmail.com>
2017-12-11 22:17:15 +0300

    test

--- admin/application/controllers/test_controller.php
+++ admin/application/controllers/test_controller.php
@@ -20,18 +20,6 @@ class Categories extends CI_Controller {
 	{
 		$html = '<ul class="'.$class_name.'">';
 		
-		foreach ($categories as $category) {
-			$glyphs = 	'<a cat_id="' . $category['id'] . '" class="color-black edit-category" href="#" style="text-decoration: none">
-						<span class="glyphicon glyphicon-pencil"></span>
-					</a>
-					<a cat_id="' . $category['id'] . '" class="delete-category color-black" href="#" style="text-decoration: none">
-						<span class="glyphicon glyphicon-remove"></span>
-					</a>';
-			$html .= '<li>' . $category['title'] . $glyphs . '</li>'; 
-			if (count($category['child_categories'])) {
-				$html .= $this->prepareHtmlForCategoriesList($category['child_categories'], 'child_category');
-			}
-		}
 		$html .= '</ul>';
 		return $html;
 	}
